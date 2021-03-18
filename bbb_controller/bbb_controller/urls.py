3cda3bc967c45d94ead78a01049f30e07701c4b4
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-03-18 01:22:35 +0100

    Drafted admin page for api calls

--- bbb_controller/bbb_controller/urls.py
+++ bbb_controller/bbb_controller/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from children.views import MakeCallsView
+
+
 urlpatterns = [
+    path("admin/calls", MakeCallsView.as_view()),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
 ]
--- bbb_controller/children/views.py
+++ bbb_controller/children/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
+import os
 
-# Create your views here.
+import requests
+from django.conf import settings
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from rc_protocol import get_checksum
+
+
+class MakeCallsView(LoginRequiredMixin, TemplateView):
+
+    template_name = "calls.html"
+
+    def get(self, request, *args, **kwargs):
+        method = request.GET.get("method", "post")
+        url = request.GET.get("url", None)
+        secret = request.GET.get("secret", None)
+        parameters = json.loads(request.GET.get("parameters", "null"))
+
+        if url and secret and parameters:
+            parameters["checksum"] = get_checksum(parameters, secret, os.path.basename(url))
+            response = requests.request(method, url, json=parameters, verify=settings.VERIFY_SSL_CERTS)
+        else:
+            response = None
+
+        return render(request, self.template_name, context={
+            "response": response
+        })
