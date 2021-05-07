65ed5b0d598034b48f1e3cffeaf59053bd8f0976
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-05-07 18:25:37 +0200

    Added channel-frontend-relation model to admin page

--- bbb_controller/api/admin.py
+++ bbb_controller/api/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from api.models import Channel
+from api.models import Channel, Channel2Frontend
+
+
+@admin.register(Channel2Frontend)
+class C2FAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Channel)
